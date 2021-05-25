9743ae8b38bec379d6210dc495354fa7f3f3d939
Nirvaan <justthingslike420@gmail.com>
2021-05-24 19:27:48 -0700

    ground color change

--- ground.js
+++ ground.js
@@ -21,8 +21,8 @@ class ground
 			push()
 			translate(groundPos.x, groundPos.y);
 			rectMode(CENTER)
-			//strokeWeight(4);
-			fill(128,128,128)
+			strokeWeight(4);
+			fill("red");
 			rect(0,0,this.w, this.h);
 			pop()
 			
